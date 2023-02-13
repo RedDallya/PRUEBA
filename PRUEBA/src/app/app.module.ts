bd11e779271ca67c5bb34094279abc7a38f20ca1
pia salas <elbosquecito30@gmail.com>
2023-02-13 15:25:20 -0300

    creacion de los dos primeros components

--- PRUEBA/src/app/app.module.ts
+++ PRUEBA/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
