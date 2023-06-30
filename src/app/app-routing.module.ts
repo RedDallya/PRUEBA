7ca58c1bdf6e4627d690b75c3be3f6ad20204173
pia salas <elbosquecito30@gmail.com>
2023-06-29 22:17:54 -0300

    mas arreglos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { NewExperienciaComponent } from './components/experiencia/new-experienci
 import { EditExperienciaComponent } from './components/experiencia/edit-experiencia/edit-experiencia.component';
 import { EditeducacionComponent } from './components/educacion/editeducacion.component';
 import { NeweducacionComponent } from './components/educacion/neweducacion.component';
+import { NewSkillComponent } from './components/skills/new-skill.component';
+import { EditSkillComponent } from './components/skills/edit-skill.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
@@ -13,10 +15,12 @@ const routes: Routes = [
   {path: 'nuevaexp', component: NewExperienciaComponent},
 {path: 'editexp/:id', component: EditExperienciaComponent},
 { path: 'nuevaedu', component: NeweducacionComponent},
-{ path: 'editedu/:id', component: EditeducacionComponent}
+{ path: 'editedu/:id', component: EditeducacionComponent},
+{ path: 'newskill', component: NewSkillComponent},
+{ path: 'editskill/:id', component: EditSkillComponent}
 ];
 
-@NgModule({
+@NgModule ({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
--- src/app/model/persona.model.ts
+++ src/app/model/persona.model.ts
@@ -2,14 +2,12 @@ export class persona {
     id?: number;
     nombre: string;
     apellido: string;
-    profesion: string;
-    descripcion: string;
     img: string;
 
-    constructor(nombre: string, apellido: string, profesion: string,) {
+    constructor(nombre: string, apellido: string, img: string,) {
         this.nombre = nombre;
         this.apellido = apellido;
-        this.profesion = profesion;
+        this.img = img;
 
     }
 }
